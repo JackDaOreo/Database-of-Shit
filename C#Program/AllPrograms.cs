2e71be8d768ff61935bd812f08e4da43bde1d870
Jack <73403239+JackDaOreo@users.noreply.github.com>
2020-10-24 21:47:41 -0400

    Add files via upload

---
+++ C#Program/AllPrograms.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace C_Program
+{
+    class AllPrograms
+    {
+        static public void PhallicMeasurement()
+        {
+            double length;
+
+            //Input
+            while (true){
+                Console.Write("What is the size of your penis (in): ");
+                try
+                {
+                    length = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+
+                //Percent calc
+                double percent = 5.4 - length;
+                percent = percent / 5.4;
+                percent = percent * 100;
+                percent = Math.Round(percent, 2);
+                percent = Math.Abs(percent);
+                //End calc
+
+                if (length > 5.4) {
+                    Console.Write("\nYour lying. ");
+                } else if (length < 5.4) {
+                    Console.WriteLine("\nThank you for your honesty. Your penis size is now saved in the database.\n\nFun fact: your penis is " + percent +"% below the average.");
+                    break;
+                } else {
+                    Console.WriteLine("\nThank you for your honesty. Your penis size is now saved in the database.\n\nFun fact: your penis is exactly average.");
+                    break;
+                }
+            }
+        }
+
+        static public void VenturaHighway()
+        {
+            Random rnd = new Random();
+            int vid = rnd.Next(1,11);
+            if (vid <= 2) {
+                //rick
+                System.Diagnostics.Process.Start("cmd","/c start https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+            } else {
+                //ventura
+                System.Diagnostics.Process.Start("cmd","/c start https://www.youtube.com/watch?v=tnV7dTXlXxs");
+            }
+        }
+
+        static public void IsPrimeNumber()
+        {
+            bool ans = false;
+            int input;
+
+            //Input
+            Console.Write("\nEnter an integer: ");
+            try
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+
+            //Exceptions
+            switch(input) {
+                case 1:
+                    ans = false;
+                    break;
+                case 2:
+                    ans = true;
+                    break;
+                case 3:
+                    ans = true;
+                    break;
+                case 5:
+                    ans = true;
+                    break;
+                default:
+                    break;
+            }
+
+            //test if prime number
+            for (int i = input - 1; i > 1; i--) {  
+                if (input % i == 0) {
+                    ans = false;
+                    break;
+                } else {
+                    ans = true;
+                }
+            }
+
+            //Output
+            if (ans == false) {
+                Console.WriteLine("\n" + input + " is not a prime number.");
+            } else {
+                Console.WriteLine("\n" + input + " is a prime number.");
+            }
+        }
+    }
+}
---
+++ C#Program/MainMenu.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading; 
+
+namespace C_Program
+{
+    class MainMenu
+    {
+        static void Main(string[] args)
+        {
+            //Directory code
+            while (true) {
+                int directory = 0;
+                Console.WriteLine("(0) Exit\n(1) Phallic Measurement\n(2) EMPTY\n(3) Play Ventura Highway\n(4) Prime Number Detector");
+                Console.Write("Type the number of the program to execute: ");
+                try
+                {
+                    directory = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine("\nError: " + ex.Message);
+                }
+                switch (directory) {
+                    //Exit
+                    case 0:
+                        System.Environment.Exit(1);
+                        break;
+                    //Phallic Measurement
+                    case 1:
+                        try
+                        {
+                            AllPrograms.PhallicMeasurement();
+                        }
+                        catch (System.Exception ex)
+                        {
+                            Console.WriteLine("\nError: " + ex.Message);
+                        }
+                        break;
+                    //EMPTY
+                    case 2:
+                        break;
+                    //Ventura Highway
+                    case 3:
+                        AllPrograms.VenturaHighway();
+                        break;
+                    //Prime Number Detector
+                    case 4:
+                        try
+                        {
+                            AllPrograms.IsPrimeNumber();
+                        }
+                        catch (System.Exception ex)
+                        {
+                            Console.WriteLine("\nError: " + ex.Message);
+                        }
+                        break;
+                    default:
+                        Console.Write("\nEnter a valid number.");
+                        break;
+                }
+
+                //Wait to see result
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+    }
+}

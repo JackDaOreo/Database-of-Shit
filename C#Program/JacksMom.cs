9afa1833b553ad94e6db341f0eb8cc17878bd9e0
thelegend32 <73374132+thelegend32@users.noreply.github.com>
2020-10-25 21:04:39 -0400

    Create JacksMom.cs

---
+++ C#Program/JacksMom.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace C_Program
+{
+    class JacksMom
+    {
+        static public void JackMom() {
+
+            Console.WriteLine("Welcome to JacksMom, an interactive experience.");
+            Console.Write("Input something: ");
+            string x = Console.ReadLine();
+            Console.WriteLine(x);
+        }
+    }
+
+
+}
